9f910c9c7545ae51b0ec6a6a787abe8f5ca1a9a3
kot-vmeshke <kot.vmeshke.4@gmail.com>
2021-08-03 02:25:58 +0300

    feat: add hundreds

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 module.exports = function toReadable (number) {
   let units = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
-      teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+      teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'],
+      tens = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
   if (number == 0) {
     return 'zero';
   } else if (number < 10) {
     return units[number-1];
   } else if (number >= 10 && number < 20) {
     return teens[number%10];
+  } else if (number >= 20 && number < 100) {
+    let ten = (Math.floor(number/10))-2;
+    let unit = (number%10)-1;
+    return `${tens[ten]} ${units[unit]}`;
+  } else if (number%100 == 0) {
+    return `${units[(number/100)-1]} hundred`;
   }
 }
