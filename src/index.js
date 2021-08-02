acda3d63451d660c3b0d66adb2c48d3e72949511
kot-vmeshke <kot.vmeshke.4@gmail.com>
2021-08-03 02:14:18 +0300

    feat: add 0-99

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
+  let units = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+      teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  if (number == 0) {
+    return 'zero';
+  } else if (number < 10) {
+    return units[number-1];
+  } else if (number >= 10 && number < 20) {
+    return teens[number%10];
+  }
 }
